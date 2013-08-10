f322a901a060f62deda868ba4b8c3ff021ba8700
Dan Ward <dan.j.ward@gmail.com>
2013-08-10 18:57:39 +0100

    brute force problem 6

---
+++ Problem6/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    let n = [1..100]
+    let ``sum of squares`` = n |> Seq.map (fun x -> x*x)
+                                |> Seq.sum
+    let sum = n |> Seq.sum
+    let ``square of sum`` = sum * sum
+              
+    printfn "square of sum - sum of squares = %A - %A = %A" ``square of sum`` ``sum of squares`` (``square of sum`` - ``sum of squares``)
+    0 // return an integer exit code
